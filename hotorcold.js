3e3322a43f79e816ce59a09f9dab0b4522ef061d
Deryx <deryxescott@yahoo.com>
2014-03-08 17:41:25 -0500

    Update hotorcold.js

--- hotorcold.js
+++ hotorcold.js
@@ -13,7 +13,7 @@ var gold = "#D4A017";
 
 $(document).ready(function() {
 	
-	$("#status").text( "Let's Play!!!" );
+	$("#status").text( "Pick a number between 1 and 100..." );
 	   
 	$( "#submitButton" ).on( "click", function( e ) 
 	{
